e8c99639fbc3a22dabb61c36b94610457fe27df5
Ryan Delaney <rrdelaney@outlook.com>
2016-02-19 17:07:21 -0500

    fixed lint

--- src/api.js
+++ src/api.js
@@ -1,10 +1,10 @@
-const req = host => ({
-  get: path => fetch(`${host}/${path}`).then(res => res.json()),
+const req = (host) => ({
+  get: (path) => fetch(`${host}/${path}`).then((res) => res.json()),
   post: (path, body) => fetch(host, {
     method: 'post',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(body)
-  }).then(res => res.json())
+  }).then((res) => res.json())
 })
 
 export const SF_API = req(process.env.NODE_ENV !== 'production'
